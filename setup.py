5aea0143759b0fbd0a3094c8e4e9948dffaf93e6
Sebastien Besson <seb.besson@gmail.com>
2017-03-13 10:39:07 +0000

    Bump VERSION in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools.command.test import test as TestCommand
 import multiprocessing
 assert multiprocessing  # silence flake8
 
-VERSION = '0.4.1'
+VERSION = '0.5.0'
 
 
 def get_requirements(suffix=''):
