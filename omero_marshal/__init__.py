6a0c9800990b833a43c700cd5bacc74a7adb21ac
Jean-Marie Burel <j.burel@dundee.ac.uk>
2018-12-12 21:43:44 +0000

    Drop support for 5.1.x

--- omero_marshal/__init__.py
+++ omero_marshal/__init__.py
@@ -53,7 +53,7 @@ def get_schema_version(version):
     if m is None:
         raise Exception("Invalid OMERO version number: " + version)
     v = StrictVersion(m.group(1))
-    if v >= StrictVersion('5.1.0') and v < StrictVersion('5.3.0'):
+    if v >= StrictVersion('5.2.0'):
         return '2015-01'
     elif v >= StrictVersion('5.3.0') and v < StrictVersion('6.0.0'):
         return '2016-06'
